b07d4921b4a0a8d4e275a4b40010cd2b6f9633ac
Anson Low Z.F <zhanfaye@gmail.com>
2021-06-23 21:36:19 +0800

    initial appjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
+import * as React from "react";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { CacheProvider } from "@emotion/react";
+import { CssBaseline } from "@material-ui/core";
+import createCache from "@emotion/cache";
+import theme from "../styles/theme";
+import "/styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export const cache = createCache({ key: "css", prepend: true });
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <CacheProvider value={cache}>
+      <Head>
+        <title>Stone Innovations Enterprise</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
-
-export default MyApp
