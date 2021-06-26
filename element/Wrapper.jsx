87321271c1be000e418de59d9b8f2620638f3a97
Anson Low Z.F <zhanfaye@gmail.com>
2021-06-26 18:07:01 +0800

    add text wrapper

--- element/Wrapper.jsx
+++ element/Wrapper.jsx
@@ -1,7 +1,13 @@
-import { Box } from "@material-ui/core";
+import { Box, Container } from "@material-ui/core";
 
 export const SectionWrapper = ({ children }) => (
   <Box component="section" sx={{ mb: { xs: 15, sm: 20 } }}>
     {children}
   </Box>
+);
+
+export const TextWrapper = ({ children }) => (
+  <Container component="article" maxWidth="sm">
+    {children}
+  </Container>
 );
