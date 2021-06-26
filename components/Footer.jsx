576bab3308db57874f3f85e936d43045d71007ce
Anson Low Z.F <zhanfaye@gmail.com>
2021-06-26 13:37:19 +0800

    footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,52 @@
+import { Box, Typography, Stack } from "@material-ui/core";
+import { Facebook, Instagram, Twitter, Email } from "@material-ui/icons";
+
+const Footer = () => {
+  return (
+    <Box component="footer" sx={{ py: 10, bgcolor: "primary.main" }}>
+      <Stack direction="row" justifyContent="center" spacing={4} sx={{ mb: 8 }}>
+        <Box
+          component="a"
+          sx={{ textDecoration: "none", color: "common.white" }}
+          href="YourFacebookLink"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Facebook fontSize="large" />
+        </Box>
+        <Box
+          component="a"
+          sx={{ textDecoration: "none", color: "common.white" }}
+          href="YourInstagramLink"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Instagram fontSize="large" />
+        </Box>
+        <Box
+          component="a"
+          sx={{ textDecoration: "none", color: "common.white" }}
+          href="YourTwitterLink"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Twitter fontSize="large" />
+        </Box>
+        <Box
+          component="a"
+          sx={{ textDecoration: "none", color: "common.white" }}
+          href="YourEmailLink"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Email fontSize="large" />
+        </Box>
+      </Stack>
+      <Typography align="center" color="common.white">
+        © 2013 - {new Date().getFullYear()}, Stone Innovations Enterprise
+      </Typography>
+    </Box>
+  );
+};
+
+export default Footer;
