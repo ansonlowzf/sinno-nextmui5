c3038531eaec9c2e1b55015d474d2956eb539447
Anson Low Z.F <zhanfaye@gmail.com>
2021-06-29 09:22:46 +0800

    add warranty section

---
+++ components/QuartzStone/SectionWarranty.jsx
@@ -0,0 +1,24 @@
+import { SectionWrapper, Heading2, TextWrapper } from "../../element";
+import { SinnoButton } from "../../components";
+import { Typography } from "@material-ui/core";
+
+export const SectionWarranty = () => (
+  <SectionWrapper>
+    <Heading2>Warranty</Heading2>
+    <TextWrapper>
+      <Typography align="center" paragraph>
+        What if the quartz countertop you bought from us is crack, chip-off,
+        mildew growth, colour dimming without any impact?
+      </Typography>
+      <Typography component="p" variant="body1" align="center" paragraph>
+        First, check out the warranty1
+      </Typography>
+      <SectionWrapper>
+        <SinnoButton linkUrl="/blog/warranty" btnText="Warranty" />
+      </SectionWrapper>
+      <Typography variant="h6" align="center">
+        If under warranty, we replace a new set for you - FREE OF CHARGE!
+      </Typography>
+    </TextWrapper>
+  </SectionWrapper>
+);
