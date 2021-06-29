00ac485f58e296bc17ee9267edb24c7681ea82b5
Anson Low Z.F <zhanfaye@gmail.com>
2021-06-29 15:14:50 +0800

    add zenstone logo

--- pages/quartz-stone/zenstone.js
+++ pages/quartz-stone/zenstone.js
@@ -1,6 +1,8 @@
-import { Hero } from "../../components";
+import { Hero, SubNavbar } from "../../components";
 import { qsRoutes } from "../../const/nav-links";
-import { SubNavbar } from "../../components";
+import { SectionWrapper } from "../../element";
+import { Grid } from "@material-ui/core";
+import Image from "next/image";
 
 const ZenstonePage = () => {
   return (
@@ -13,6 +15,20 @@ const ZenstonePage = () => {
       />
 
       <SubNavbar routes={qsRoutes} />
+
+      <SectionWrapper>
+        <Grid container justifyContent="center" sx={{ mt: 15 }}>
+          <Grid item xs={10} md={2}>
+            <Image
+              src="/image-zenstone/logo.png"
+              alt="zenstone logo"
+              layout="responsive"
+              width={800}
+              height={600}
+            />
+          </Grid>
+        </Grid>
+      </SectionWrapper>
     </>
   );
 };
