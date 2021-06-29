e454df979190dc4f01df0b06f8c01183a9b5a34d
Anson Low Z.F <zhanfaye@gmail.com>
2021-06-29 09:23:33 +0800

    add component

---
+++ components/Benefit.jsx
@@ -0,0 +1,24 @@
+import { Grid, Typography } from "@material-ui/core";
+import Image from "next/image";
+
+export const Benefit = ({ imgSrc, title, desc }) => {
+  return (
+    <>
+      <Grid container item xs={12} md={4}>
+        <Grid item xs={12}>
+          <Image
+            src={imgSrc}
+            alt={title}
+            layout="responsive"
+            width={400}
+            height={250}
+          />
+          <Typography component="h3" variant="h6" sx={{ mt: 1 }} gutterBottom>
+            {title}
+          </Typography>
+          <Typography align="justify">{desc}</Typography>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
