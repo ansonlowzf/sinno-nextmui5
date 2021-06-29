f8dfe816bdbd84b8f4df78b8df9c973a6ad369cf
Anson Low Z.F <zhanfaye@gmail.com>
2021-06-29 14:44:16 +0800

    add zenstone hero

---
+++ pages/quartz-stone/zenstone.js
@@ -0,0 +1,14 @@
+import { Hero } from "../../components";
+
+const ZenstonePage = () => {
+  return (
+    <Hero
+      imgSrc="/image-zenstone/hero.png"
+      imgText="Zenstone Calacatta Black"
+      title="Zenstone Malaysia"
+      subtitle="Malaysia's Brand OEM China Genuine Quartz Stone"
+    />
+  );
+};
+
+export default ZenstonePage;
