cbea8430d6766e68967a0e815c061e3e9795c90b
Anson Low Z.F <zhanfaye@gmail.com>
2021-07-02 13:54:02 +0800

    add dekton colour

--- components/DisplayStone.jsx
+++ components/DisplayStone.jsx
@@ -5,7 +5,7 @@ export const DisplayStone = ({ imgSrc, stoneName, price }) => {
   return (
     <Grid item xs={12} md={6}>
       <Image src={imgSrc} alt={stoneName} width={620} height={350} />
-      <Grid container item justifyContent="center" sx={{ mt: 1, mb: 3 }}>
+      <Grid container item justifyContent="center" sx={{ mb: 3 }}>
         <Grid item xs={12} sm={6}>
           <Typography component="p" variant="subtitle2" align="center">
             {stoneName}
--- pages/sintered-stone/dekton.js
+++ pages/sintered-stone/dekton.js
@@ -1,8 +1,15 @@
-import { Box } from "@material-ui/core";
-import { Hero, SubNavbar } from "../../components";
+import { Box, Container, Grid } from "@material-ui/core";
+import {
+  Hero,
+  SubNavbar,
+  DisplayStone,
+  ColourWarning,
+  PriceTerm,
+  SectionContact,
+} from "../../components";
 import { dekton12 } from "../../const/pricing";
 import { ssRoutes } from "../../const/nav-links";
-import { Heading2 } from "../../element";
+import { Heading2, SectionWrapper } from "../../element";
 
 const { group0, group1, group2, group3, group4 } = dekton12;
 
@@ -20,6 +27,263 @@ const DektonPage = () => {
       <Box sx={{ my: { xs: 15, sm: 20 } }}>
         <Heading2>Colour &amp; Pricing</Heading2>
       </Box>
+
+      <SectionWrapper>
+        <Container maxWidth="lg">
+          <Grid container spacing={2} justifyContent="">
+            <DisplayStone
+              imgSrc="/dekton/ucs/aeris.png"
+              stoneName="Aeris (Smooth Matt) 12mm"
+              price={group0}
+            />
+            <DisplayStone
+              imgSrc="/dekton/ucs/aura15.png"
+              stoneName="Aura 15 (Smooth Matt) 12mm"
+              price={group3}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/baltic.png"
+              stoneName="Baltic (Smooth Matt) 12mm"
+              price={group3}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/blanc-concrete.png"
+              stoneName="Blanc Concrete (Texture) 12mm"
+              price={group0}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/bromo.png"
+              stoneName="Bromo (Smooth Matt) 12mm"
+              price={group1}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/danae.png"
+              stoneName="Danae (Smooth Matt) 12mm"
+              price={group0}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/domoos.png"
+              stoneName="Domoos (Smooth Matt) 12mm"
+              price={group2}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/edora.png"
+              stoneName="Edora (Texture) 12mm"
+              price={group1}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/entzo.png"
+              stoneName="Entzo (Smooth Matt) 12mm"
+              price={group3}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/eter.png"
+              stoneName="Eter (Smooth Matt) 12mm"
+              price={group0}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/feroe.png"
+              stoneName="Feroe (Smooth Matt) 12mm"
+              price={group3}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/fossil.png"
+              stoneName="Fossil (Smooth Matt) 12mm"
+              price={group1}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/kairos.png"
+              stoneName="Kairos (Smooth Matt) 12mm"
+              price={group2}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/kelya.png"
+              stoneName="Kelya (Smooth Matt) 12mm"
+              price={group2}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/keon.png"
+              stoneName="Keon (Smooth Matt) 12mm"
+              price={group0}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/keranium.png"
+              stoneName="Kenanium (Smooth Matt) 12mm"
+              price={group1}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/kira.png"
+              stoneName="Kira (Smooth Matt) 12mm"
+              price={group2}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/kovik.png"
+              stoneName="Kovik (Smooth Matt) 12mm"
+              price={group2}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/kreta.png"
+              stoneName="Kreta (Smooth Matt) 12mm"
+              price={group2}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/laos.png"
+              stoneName="Laos (Smooth Matt) 12mm"
+              price={group2}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/laurent.png"
+              stoneName="Laurent (Smooth Matt) 12mm"
+              price={group3}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/liquid-embers.png"
+              stoneName="Liquid Embers (Texture) 12mm"
+              price={group3}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/liquid-shell.png"
+              stoneName="Liquid Shell (Texture) 12mm"
+              price={group3}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/liquid-sky.png"
+              stoneName="Liquid Sky (Texture) 12mm"
+              price={group3}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/lunar.png"
+              stoneName="Lunar (Smooth Matt) 12mm"
+              price={group2}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/milar.png"
+              stoneName="Milar (Smooth Matt) 12mm"
+              price={group0}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/nayla.png"
+              stoneName="Nayla (Smooth Matt) 12mm"
+              price={group1}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/nilium.png"
+              stoneName="Nilium (Smooth Matt) 12mm"
+              price={group2}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/opera.png"
+              stoneName="Opera (Velvet) 12mm"
+              price={group3}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/orix.png"
+              stoneName="Orix (Smooth Matt) 12mm"
+              price={group2}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/radium.png"
+              stoneName="Radium (Smooth Matt) 12mm"
+              price={group2}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/rem.png"
+              stoneName="Rem (Smooth Matt) 12mm"
+              price={group3}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/sasea.png"
+              stoneName="Sasea (Smooth Matt) 12mm"
+              price={group1}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/sirius.png"
+              stoneName="Sirius (Texture) 12mm"
+              price={group1}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/sirocco.png"
+              stoneName="Sirocco (Smooth Matt) 12mm"
+              price={group0}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/soke.png"
+              stoneName="Soke (Smooth Matt) 12mm"
+              price={group2}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/trilium.png"
+              stoneName="Trilium (Smooth Matt) 12mm"
+              price={group2}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/uyuni.png"
+              stoneName="Uyuni (Smooth Matt) 12mm"
+              price={group3}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/vera.png"
+              stoneName="Vera (Smooth Matt) 12mm"
+              price={group2}
+            />
+
+            <DisplayStone
+              imgSrc="/dekton/ucs/zenith.png"
+              stoneName="Zenith (Smooth Matt) 12mm"
+              price={group2}
+            />
+          </Grid>
+        </Container>
+      </SectionWrapper>
+
+      <SectionWrapper>
+        <ColourWarning />
+      </SectionWrapper>
+
+      <SectionWrapper>
+        <PriceTerm />
+      </SectionWrapper>
+
+      <SectionWrapper>
+        <SectionContact stone="Dekton" />
+      </SectionWrapper>
     </>
   );
 };
