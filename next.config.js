2995c841efbe023a2e38b4553b426195fc05a553
Anson Low Z.F <zhanfaye@gmail.com>
2021-07-03 15:56:35 +0800

    add generate sitemap

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./lib/generate-sitemap");
+    }
+
+    return config;
+  },
+};
