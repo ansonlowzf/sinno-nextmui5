dddce8ebd15824308e6988a992cc3cb11b2c1665
Anson Low Z.F <zhanfaye@gmail.com>
2021-07-15 16:02:08 +0800

    remove useful code

--- pages/_app.js
+++ pages/_app.js
@@ -18,14 +18,6 @@ const cache = createCache({ key: "css" });
 cache.compat = true;
 
 export default function MyApp({ Component, pageProps }) {
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-
   const router = useRouter();
 
   React.useEffect(() => {
