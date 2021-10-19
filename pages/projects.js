da408158aaf939810fac4c7436ffb20131e7ebeb
Anson Low Z.F <zhanfaye@gmail.com>
2021-10-19 16:22:05 +0800

    add project

--- pages/projects.js
+++ pages/projects.js
@@ -34,6 +34,7 @@ const Project = () => {
               height={600}
             />
           </Grid>
+
           <Grid item xs={12} md={4}>
             <Image
               src="/project/aura15/2.png"
@@ -81,6 +82,7 @@ const Project = () => {
           </Typography>
         </Container>
         <hr />
+
         <Grid container spacing={0.5} sx={{ mt: 5 }}>
           <Grid item xs={12} md={4}>
             <Image
@@ -111,6 +113,7 @@ const Project = () => {
           </Typography>
         </Container>
         <hr />
+
         <Grid container spacing={0.5} sx={{ mt: 5 }}>
           <Grid item xs={12} md={4}>
             <Image
@@ -159,6 +162,7 @@ const Project = () => {
           </Typography>
         </Container>
         <hr />
+
         <Grid container spacing={0.5} sx={{ mt: 5 }}>
           <Grid item xs={12} md={4}>
             <Image
@@ -202,6 +206,7 @@ const Project = () => {
           </Typography>
         </Container>
         <hr />
+
         <Grid container spacing={0.5} sx={{ mt: 5 }}>
           <Grid item xs={12} md={4}>
             <Image
@@ -267,6 +272,138 @@ const Project = () => {
             &amp; overhang island top for breakfast &amp; dining purposes.
           </Typography>
         </Container>
+        <hr />
+
+        <Grid container spacing={0.5} sx={{ mt: 5 }}>
+          <Grid item xs={12} md={4}>
+            <Image
+              src="/project/blue-pearl/1.png"
+              layout="responsive"
+              alt="caesarstone pure grey kitchen top"
+              width={800}
+              height={600}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Image
+              src="/project/blue-pearl/2.png"
+              layout="responsive"
+              alt="caesarstone pure grey kitchen top"
+              width={800}
+              height={600}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Image
+              src="/project/blue-pearl/3.png"
+              layout="responsive"
+              alt="caesarstone pure grey kitchen top"
+              width={800}
+              height={600}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Image
+              src="/project/blue-pearl/4.png"
+              layout="responsive"
+              alt="caesarstone pure grey kitchen top"
+              width={800}
+              height={600}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Image
+              src="/project/blue-pearl/5.png"
+              layout="responsive"
+              alt="caesarstone pure grey kitchen top"
+              width={800}
+              height={600}
+            />
+          </Grid>
+        </Grid>
+        <Container maxWidth="sm" sx={{ my: 5 }}>
+          <Typography fontWeight="bold" gutterBottom>
+            Work Description:
+          </Typography>
+          <Typography paragraph>
+            Supply &amp; install 15mm granite Blue Pearl kitchen top
+          </Typography>
+        </Container>
+        <hr />
+
+        <Grid container spacing={0.5} sx={{ mt: 5 }}>
+          <Grid item xs={12} md={4}>
+            <Image
+              src="/project/crema-marfil/1.png"
+              layout="responsive"
+              alt="volakas marble countertop"
+              width={800}
+              height={600}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Image
+              src="/project/crema-marfil/2.png"
+              layout="responsive"
+              alt="volakas marble countertop"
+              width={800}
+              height={600}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Image
+              src="/project/crema-marfil/3.png"
+              layout="responsive"
+              alt="volakas marble countertop"
+              width={800}
+              height={600}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Image
+              src="/project/crema-marfil/4.png"
+              layout="responsive"
+              alt="volakas marble countertop"
+              width={800}
+              height={600}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Image
+              src="/project/crema-marfil/5.png"
+              layout="responsive"
+              alt="volakas marble countertop"
+              width={800}
+              height={600}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Image
+              src="/project/crema-marfil/6.png"
+              layout="responsive"
+              alt="volakas marble countertop"
+              width={800}
+              height={600}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Image
+              src="/project/crema-marfil/7.png"
+              layout="responsive"
+              alt="volakas marble countertop"
+              width={800}
+              height={600}
+            />
+          </Grid>
+        </Grid>
+        <Container maxWidth="sm" sx={{ my: 5 }}>
+          <Typography fontWeight="bold" gutterBottom>
+            Work Description:
+          </Typography>
+          <Typography paragraph>
+            Supply &amp; install marble Creme Marfil living hall flooring.
+          </Typography>
+        </Container>
 
         <Container maxWidth="sm" sx={{ my: 5 }}>
           <Heading3>More Completed Projects</Heading3>
@@ -277,7 +414,7 @@ const Project = () => {
         </Container>
       </Container>
 
-      <Container maxWidth="lg" sx={{ mt: 5 }}>
+      <Container maxWidth="lg" sx={{ mt: 15 }}>
         <SectionWrapper>
           <Heading2>Commercial Projects</Heading2>
 
@@ -436,6 +573,39 @@ const Project = () => {
               countertop.
             </Typography>
           </Container>
+          <hr />
+
+          <Grid container spacing={0.5} sx={{ mt: 5 }}>
+            <Grid item xs={12} md={4}>
+              <Image
+                src="/project/volakas-marble/1.png"
+                layout="responsive"
+                alt="volakas marble countertop"
+                width={800}
+                height={600}
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Image
+                src="/project/volakas-marble/2.png"
+                layout="responsive"
+                alt="volakas marble countertop"
+                width={800}
+                height={600}
+              />
+            </Grid>
+          </Grid>
+          <Container maxWidth="sm" sx={{ my: 5 }}>
+            <Typography fontWeight="bold" gutterBottom>
+              Work Description:
+            </Typography>
+            <Typography paragraph>
+              Supply &amp; install Volakas marble reception countertop. (Not
+              recommend, client accept the disadvantages of the marble to
+              exchange of the beauty.)
+            </Typography>
+          </Container>
+
           <Container maxWidth="sm" sx={{ my: 5 }}>
             <Heading3>More Completed Projects</Heading3>
             <Typography paragraph>
