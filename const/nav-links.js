a27860008b26116e27b08cfe84d41297f0858839
Anson Low Z.F <zhanfaye@gmail.com>
2021-12-08 19:21:39 +0800

    change link

--- const/nav-links.js
+++ const/nav-links.js
@@ -1,7 +1,7 @@
 export const mainRoutes = [
   { name: "home", path: "/" },
   { name: "quartz stone", path: "/quartz-stone" },
-  { name: "sintered stone", path: "/sintered-stone" },
+  { name: "sintered stone", path: "/sintered-stone/moca-compact" },
   { name: "granite", path: "/granite" },
   { name: "marble", path: "/marble" },
   { name: "projects", path: "/projects" },
