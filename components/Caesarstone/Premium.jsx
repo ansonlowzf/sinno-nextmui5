db138ae48c462c9deeca1f16fa3bfedd47096a7b
Anson Low Z.F <zhanfaye@gmail.com>
2021-12-22 15:46:22 +0800

    caesarstone new pricing

--- components/Caesarstone/Premium.jsx
+++ components/Caesarstone/Premium.jsx
@@ -5,24 +5,29 @@ import { caesarstone20 } from "const/pricing";
 
 const { premium } = caesarstone20;
 
-const CaesarstonePremium = () => {
+const Premium = () => {
   return (
     <>
       <CollectionTitle>Premium Collection</CollectionTitle>
 
       <Grid container spacing={1} justifyContent="center">
         <DisplayStone
-          imgSrc="/caesarstone/cs/1141.png"
+          imgSrc="/caesarstone/1141.jpg"
           stoneName="1141 Pure White (Polished)"
           price={premium}
         />
         <DisplayStone
-          imgSrc="/caesarstone/cs/6131.png"
+          imgSrc="/caesarstone/5110.jpg"
+          stoneName="5110 Alpine Mist (Polished)"
+          price={premium}
+        />
+        <DisplayStone
+          imgSrc="/caesarstone/6131.jpg"
           stoneName="6131 Bianco Drift (Polished)"
           price={premium}
         />
         <DisplayStone
-          imgSrc="/caesarstone/cs/6313.png"
+          imgSrc="/caesarstone/6313.jpg"
           stoneName="6313 Turbine Grey (Polished)"
           price={premium}
         />
@@ -31,4 +36,4 @@ const CaesarstonePremium = () => {
   );
 };
 
-export default CaesarstonePremium;
+export default Premium;
--- components/Caesarstone/Standard.jsx
+++ components/Caesarstone/Standard.jsx
@@ -5,44 +5,49 @@ import { Grid } from "@mui/material";
 
 const { standard } = caesarstone20;
 
-const CaesarstoneStandard = () => {
+const Standard = () => {
   return (
     <>
       <CollectionTitle>Standard Collection</CollectionTitle>
 
       <Grid container spacing={1} justifyContent="center">
         <DisplayStone
-          imgSrc="/caesarstone/cs/2141.png"
+          imgSrc="/caesarstone/2141.jpg"
           stoneName="2141 Snow (Polished)"
           price={standard}
         />
         <DisplayStone
-          imgSrc="/caesarstone/cs/3100.png"
+          imgSrc="/caesarstone/3100.jpg"
           stoneName="3100 Jet Black (Polished)"
           price={standard}
         />
         <DisplayStone
-          imgSrc="/caesarstone/cs/4030.png"
+          imgSrc="/caesarstone/4030.jpg"
           stoneName="4030 Oyster (Polished)"
           price={standard}
         />
         <DisplayStone
-          imgSrc="/caesarstone/cs/4220.png"
+          imgSrc="/caesarstone/4130.jpg"
+          stoneName="4130 Clamshell (Polished)"
+          price={standard}
+        />
+        <DisplayStone
+          imgSrc="/caesarstone/4220.jpg"
           stoneName="4220 Buttermilk (Polished)"
           price={standard}
         />
         <DisplayStone
-          imgSrc="/caesarstone/cs/4330.png"
+          imgSrc="/caesarstone/4330.jpg"
           stoneName="4330 Ginger (Polished)"
           price={standard}
         />
         <DisplayStone
-          imgSrc="/caesarstone/cs/4350.png"
+          imgSrc="/caesarstone/4350.jpg"
           stoneName="4350 Mink (Polished)"
           price={standard}
         />
         <DisplayStone
-          imgSrc="/caesarstone/cs/6011.png"
+          imgSrc="/caesarstone/6011.jpg"
           stoneName="6011 Intense White (Polished)"
           price={standard}
         />
@@ -51,4 +56,4 @@ const CaesarstoneStandard = () => {
   );
 };
 
-export default CaesarstoneStandard;
+export default Standard;
