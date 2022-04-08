15786794dfe59c7ec0cf98ee9d217048f2719672
AnsonLowZF <zhanfaye@gmail.com>
2022-04-08 08:41:35 +0800

    remove section wrapper

--- pages/projects.js
+++ pages/projects.js
@@ -270,211 +270,209 @@ const Project = () => {
       </Container>
 
       <Container maxWidth="lg" sx={{ mt: 15 }}>
-        <SectionWrapper>
-          <Heading2>Commercial Projects</Heading2>
-          <Grid container spacing={0.5}>
-            <ProjectImage
-              imgSrc="/project/lavendar/cs/1.jpg"
-              imgAlt="Caesarstone Countertop Lavendar Bakery"
-            />
-            <ProjectImage
-              imgSrc="/project/lavendar/cs/2.jpg"
-              imgAlt="Caesarstone Countertop Lavendar Bakery"
-            />
-            <ProjectImage
-              imgSrc="/project/lavendar/cs/5.jpg"
-              imgAlt="Caesarstone Countertop Lavendar Bakery"
-            />
-            <ProjectImage
-              imgSrc="/project/lavendar/cs/6.jpg"
-              imgAlt="Caesarstone Countertop Lavendar Bakery"
-            />
-            <ProjectImage
-              imgSrc="/project/lavendar/cs/7.jpg"
-              imgAlt="Caesarstone Countertop Lavendar Bakery"
-            />
-            <ProjectImage
-              imgSrc="/project/lavendar/cs/9.jpg"
-              imgAlt="Caesarstone Countertop Lavendar Bakery"
-            />
-            <ProjectImage
-              imgSrc="/project/lavendar/cs/10.jpg"
-              imgAlt="Caesarstone Countertop Lavendar Bakery"
-            />
-            <ProjectImage
-              imgSrc="/project/lavendar/cs/12.jpg"
-              imgAlt="Caesarstone Countertop Lavendar Bakery"
-            />
-            <ProjectImage
-              imgSrc="/project/lavendar/cs/13.jpg"
-              imgAlt="Caesarstone Countertop Lavendar Bakery"
-            />
-            <ProjectImage
-              imgSrc="/project/lavendar/cs/15.jpg"
-              imgAlt="Caesarstone Countertop Lavendar Bakery"
-            />
-          </Grid>
-          <ProjectDescription
-            description="
-              Supply &amp; install Caesarstone Oyster &amp; Zenstone Fabricator
-              White Quartz Surface Bakery Countertop, Pantry Top, Cashier Top,
-              Vanity Top &amp; Worktop
-            "
+        <Heading2>Commercial Projects</Heading2>
+        <Grid container spacing={0.5}>
+          <ProjectImage
+            imgSrc="/project/lavendar/cs/1.jpg"
+            imgAlt="Caesarstone Countertop Lavendar Bakery"
           />
-
-          <hr />
-
-          <Grid container spacing={0.5}>
-            <ProjectImage
-              imgSrc="/project/cs/baskin-robin/1.jpg"
-              imgAlt="caesarstone baskin robin countertop"
-            />
-            <ProjectImage
-              imgSrc="/project/cs/baskin-robin/2.jpg"
-              imgAlt="caesarstone baskin robin countertop"
-            />
-            <ProjectImage
-              imgSrc="/project/cs/baskin-robin/3.jpg"
-              imgAlt="caesarstone baskin robin countertop"
-            />
-            <ProjectImage
-              imgSrc="/project/cs/baskin-robin/4.jpg"
-              imgAlt="caesarstone baskin robin countertop"
-            />
-            <ProjectImage
-              imgSrc="/project/cs/baskin-robin/5.jpg"
-              imgAlt="caesarstone baskin robin countertop"
-            />
-            <ProjectImage
-              imgSrc="/project/cs/baskin-robin/6.jpg"
-              imgAlt="caesarstone baskin robin countertop"
-            />
-            <ProjectImage
-              imgSrc="/project/cs/baskin-robin/7.jpg"
-              imgAlt="caesarstone baskin robin countertop"
-            />
-            <ProjectImage
-              imgSrc="/project/cs/baskin-robin/8.jpg"
-              imgAlt="caesarstone baskin robin countertop"
-            />
-          </Grid>
-          <ProjectDescription
-            description="
-              Supply &amp; install Caesarstone Raw Concrete Quartz Surface Ice
-              Cream &amp; Cashier Countertop
-            "
+          <ProjectImage
+            imgSrc="/project/lavendar/cs/2.jpg"
+            imgAlt="Caesarstone Countertop Lavendar Bakery"
           />
-
-          <hr />
-
-          <Grid container spacing={0.5}>
-            <ProjectImage
-              imgSrc="/project/klia2/1.png"
-              imgAlt="quartz stone dining table top"
-            />
-            <ProjectImage
-              imgSrc="/project/klia2/2.png"
-              imgAlt="quartz stone dining table top"
-            />
-            <ProjectImage
-              imgSrc="/project/klia2/3.png"
-              imgAlt="quartz stone dining table top"
-            />
-            <ProjectImage
-              imgSrc="/project/klia2/4.png"
-              imgAlt="quartz stone dining table top"
-            />
-            <ProjectImage
-              imgSrc="/project/klia2/5.png"
-              imgAlt="quartz stone dining table top"
-            />
-            <ProjectImage
-              imgSrc="/project/klia2/6.png"
-              imgAlt="quartz stone dining table top"
-            />
-          </Grid>
-          <ProjectDescription
-            description="
-              Supply &amp; install Zenstone Smoothie White quartz surfaces long
-              dining &amp; round dining table top. (client supply the table
-              legs)
-            "
+          <ProjectImage
+            imgSrc="/project/lavendar/cs/5.jpg"
+            imgAlt="Caesarstone Countertop Lavendar Bakery"
           />
-
-          <hr />
-
-          <Grid container spacing={0.5} sx={{ mt: 5 }}>
-            <ProjectImage
-              imgSrc="/project/yogu/1.png"
-              imgAlt="quartz stone dining table top"
-            />
-            <ProjectImage
-              imgSrc="/project/yogu/2.png"
-              imgAlt="quartz stone dining table top"
-            />
-            <ProjectImage
-              imgSrc="/project/yogu/3.png"
-              imgAlt="quartz stone dining table top"
-            />
-            <ProjectImage
-              imgSrc="/project/yogu/4.png"
-              imgAlt="quartz stone dining table top"
-            />
-          </Grid>
-          <ProjectDescription
-            description="
-              Supply &amp; install Zenstone Smoothie White quartz surfaces
-              counter, saucer and cashier top.
-            "
+          <ProjectImage
+            imgSrc="/project/lavendar/cs/6.jpg"
+            imgAlt="Caesarstone Countertop Lavendar Bakery"
           />
-
-          <hr />
-
-          <Grid container spacing={0.5} sx={{ mt: 5 }}>
-            <ProjectImage
-              imgSrc="/project/oyster/1.png"
-              imgAlt="quartz stone dining table top"
-            />
-            <ProjectImage
-              imgSrc="/project/oyster/2.png"
-              imgAlt="quartz stone dining table top"
-            />
-          </Grid>
-          <ProjectDescription
-            description="
-              Supply &amp; install Caesarstone Oyster quartz surface security
-              countertop.
-            "
+          <ProjectImage
+            imgSrc="/project/lavendar/cs/7.jpg"
+            imgAlt="Caesarstone Countertop Lavendar Bakery"
           />
-
-          <hr />
-
-          <Grid container spacing={0.5} sx={{ mt: 5 }}>
-            <ProjectImage
-              imgSrc="/project/volakas-marble/1.png"
-              imgAlt="Sintered Coffee Table"
-            />
-            <ProjectImage
-              imgSrc="/project/volakas-marble/2.png"
-              imgAlt="Sintered Coffee Table"
-            />
-          </Grid>
-          <ProjectDescription
-            description="
-              Supply &amp; install marble Volakas reception countertop. (Not
-              recommend, client accept the disadvantages of the marble to
-              exchange of the beauty.)
-            "
+          <ProjectImage
+            imgSrc="/project/lavendar/cs/9.jpg"
+            imgAlt="Caesarstone Countertop Lavendar Bakery"
           />
+          <ProjectImage
+            imgSrc="/project/lavendar/cs/10.jpg"
+            imgAlt="Caesarstone Countertop Lavendar Bakery"
+          />
+          <ProjectImage
+            imgSrc="/project/lavendar/cs/12.jpg"
+            imgAlt="Caesarstone Countertop Lavendar Bakery"
+          />
+          <ProjectImage
+            imgSrc="/project/lavendar/cs/13.jpg"
+            imgAlt="Caesarstone Countertop Lavendar Bakery"
+          />
+          <ProjectImage
+            imgSrc="/project/lavendar/cs/15.jpg"
+            imgAlt="Caesarstone Countertop Lavendar Bakery"
+          />
+        </Grid>
+        <ProjectDescription
+          description="
+            Supply &amp; install Caesarstone Oyster &amp; Zenstone Fabricator
+            White Quartz Surface Bakery Countertop, Pantry Top, Cashier Top,
+            Vanity Top &amp; Worktop
+          "
+        />
 
-          <Container maxWidth="sm" sx={{ my: 5 }}>
-            <Heading3>More Completed Projects</Heading3>
-            <Typography paragraph>
-              Find us (Stone Innovations Enterprise) on Facebook and check our
-              photo album
-            </Typography>
-          </Container>
-        </SectionWrapper>
+        <hr />
+
+        <Grid container spacing={0.5}>
+          <ProjectImage
+            imgSrc="/project/cs/baskin-robin/1.jpg"
+            imgAlt="caesarstone baskin robin countertop"
+          />
+          <ProjectImage
+            imgSrc="/project/cs/baskin-robin/2.jpg"
+            imgAlt="caesarstone baskin robin countertop"
+          />
+          <ProjectImage
+            imgSrc="/project/cs/baskin-robin/3.jpg"
+            imgAlt="caesarstone baskin robin countertop"
+          />
+          <ProjectImage
+            imgSrc="/project/cs/baskin-robin/4.jpg"
+            imgAlt="caesarstone baskin robin countertop"
+          />
+          <ProjectImage
+            imgSrc="/project/cs/baskin-robin/5.jpg"
+            imgAlt="caesarstone baskin robin countertop"
+          />
+          <ProjectImage
+            imgSrc="/project/cs/baskin-robin/6.jpg"
+            imgAlt="caesarstone baskin robin countertop"
+          />
+          <ProjectImage
+            imgSrc="/project/cs/baskin-robin/7.jpg"
+            imgAlt="caesarstone baskin robin countertop"
+          />
+          <ProjectImage
+            imgSrc="/project/cs/baskin-robin/8.jpg"
+            imgAlt="caesarstone baskin robin countertop"
+          />
+        </Grid>
+        <ProjectDescription
+          description="
+            Supply &amp; install Caesarstone Raw Concrete Quartz Surface Ice
+            Cream &amp; Cashier Countertop
+          "
+        />
+
+        <hr />
+
+        <Grid container spacing={0.5}>
+          <ProjectImage
+            imgSrc="/project/klia2/1.png"
+            imgAlt="quartz stone dining table top"
+          />
+          <ProjectImage
+            imgSrc="/project/klia2/2.png"
+            imgAlt="quartz stone dining table top"
+          />
+          <ProjectImage
+            imgSrc="/project/klia2/3.png"
+            imgAlt="quartz stone dining table top"
+          />
+          <ProjectImage
+            imgSrc="/project/klia2/4.png"
+            imgAlt="quartz stone dining table top"
+          />
+          <ProjectImage
+            imgSrc="/project/klia2/5.png"
+            imgAlt="quartz stone dining table top"
+          />
+          <ProjectImage
+            imgSrc="/project/klia2/6.png"
+            imgAlt="quartz stone dining table top"
+          />
+        </Grid>
+        <ProjectDescription
+          description="
+            Supply &amp; install Zenstone Smoothie White quartz surfaces long
+            dining &amp; round dining table top. (client supply the table
+            legs)
+          "
+        />
+
+        <hr />
+
+        <Grid container spacing={0.5} sx={{ mt: 5 }}>
+          <ProjectImage
+            imgSrc="/project/yogu/1.png"
+            imgAlt="quartz stone dining table top"
+          />
+          <ProjectImage
+            imgSrc="/project/yogu/2.png"
+            imgAlt="quartz stone dining table top"
+          />
+          <ProjectImage
+            imgSrc="/project/yogu/3.png"
+            imgAlt="quartz stone dining table top"
+          />
+          <ProjectImage
+            imgSrc="/project/yogu/4.png"
+            imgAlt="quartz stone dining table top"
+          />
+        </Grid>
+        <ProjectDescription
+          description="
+            Supply &amp; install Zenstone Smoothie White quartz surfaces
+            counter, saucer and cashier top.
+          "
+        />
+
+        <hr />
+
+        <Grid container spacing={0.5} sx={{ mt: 5 }}>
+          <ProjectImage
+            imgSrc="/project/oyster/1.png"
+            imgAlt="quartz stone dining table top"
+          />
+          <ProjectImage
+            imgSrc="/project/oyster/2.png"
+            imgAlt="quartz stone dining table top"
+          />
+        </Grid>
+        <ProjectDescription
+          description="
+            Supply &amp; install Caesarstone Oyster quartz surface security
+            countertop.
+          "
+        />
+
+        <hr />
+
+        <Grid container spacing={0.5} sx={{ mt: 5 }}>
+          <ProjectImage
+            imgSrc="/project/volakas-marble/1.png"
+            imgAlt="Sintered Coffee Table"
+          />
+          <ProjectImage
+            imgSrc="/project/volakas-marble/2.png"
+            imgAlt="Sintered Coffee Table"
+          />
+        </Grid>
+        <ProjectDescription
+          description="
+            Supply &amp; install marble Volakas reception countertop. (Not
+            recommend, client accept the disadvantages of the marble to
+            exchange of the beauty.)
+          "
+        />
+
+        <Container maxWidth="sm" sx={{ my: 5 }}>
+          <Heading3>More Completed Projects</Heading3>
+          <Typography paragraph>
+            Find us (Stone Innovations Enterprise) on Facebook and check our
+            photo album
+          </Typography>
+        </Container>
       </Container>
     </>
   );
