066ab51d2a3c6d97c8a9d7a0708c7523d2ae86d7
AnsonLowZF <zhanfaye@gmail.com>
2022-04-21 18:58:06 +0800

    update code

--- components/FrSf.jsx
+++ components/FrSf.jsx
@@ -1,29 +1,31 @@
-import { Typography, Grid } from "@mui/material";
+import { Container, Typography, Grid } from "@mui/material";
 
 const FrSf = () => {
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Typography>
-          <strong>Foot Run (FR) price</strong> is for countertop with:-
-        </Typography>
-        <Typography component="ul" sx={{ mb: 2 }}>
-          <li>Less than or equal to 600mm of depth</li>
-          <li>
-            50mm front drop/fascia/profile flat polished with a mitre joint
-          </li>
-          <li>50mm height backsplash</li>
-        </Typography>
+    <Container maxWidth="sm">
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography>
+            <strong>Foot Run (FR) price</strong> is for countertop with:-
+          </Typography>
+          <Typography component="ul" sx={{ mb: 2 }}>
+            <li>Less than or equal to 600mm of depth</li>
+            <li>
+              50mm front drop/fascia/profile flat polished with a mitre joint
+            </li>
+            <li>50mm height backsplash</li>
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography>
+            <strong>Square Feet (SF) price</strong> includes:-
+          </Typography>
+          <Typography
+            sx={{ mb: 2 }}
+          >{`Countertop exceeding the foot run total depth (700mm including backsplash and front drop) will be quoted in Square Feet price.`}</Typography>
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <Typography>
-          <strong>Square Feet (SF) price</strong> includes:-
-        </Typography>
-        <Typography
-          sx={{ mb: 2 }}
-        >{`Countertop exceeding the foot run total depth (700mm including backsplash and front drop) will be quoted in Square Feet price.`}</Typography>
-      </Grid>
-    </Grid>
+    </Container>
   );
 };
 
