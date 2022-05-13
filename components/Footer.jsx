154d589a02a58c5e01647fe24a160f4789df648f
AnsonLowZF <zhanfaye@gmail.com>
2022-05-13 20:15:39 +0800

    add credit card payment in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,38 +1,50 @@
 import { Email, Facebook, Policy } from "@mui/icons-material";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography, Grid } from "@mui/material";
+import MuiNextLink from "./MuiNextLink";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <Box component="footer" sx={{ py: 10, bgcolor: "grey.900" }}>
+      <Typography color="primary" align="center">
+        Credit Card Payment Accepted
+      </Typography>
+      <Grid
+        container
+        item
+        justifyContent="center"
+        alignItems="center"
+        sx={{ mb: 5 }}
+      >
+        <Image
+          src="/visa-mastercard.png"
+          alt="visa and mastercard logo"
+          width={128}
+          height={64}
+        />
+      </Grid>
       <Stack direction="row" justifyContent="center" spacing={4} sx={{ mb: 8 }}>
-        <Box
-          component="a"
-          sx={{ textDecoration: "none", color: "common.white" }}
+        <MuiNextLink
           href="https://www.facebook.com/SinnoENT"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Facebook fontSize="large" />
-        </Box>
-        <Box
-          component="a"
-          sx={{ textDecoration: "none", color: "common.white" }}
+        </MuiNextLink>
+
+        <MuiNextLink
           href="mailto:contact@stoneinnovations.com.my"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Email fontSize="large" />
-        </Box>
-        <Box
-          component="a"
-          sx={{ textDecoration: "none", color: "common.white" }}
-          href="/privacy-policy"
-        >
+        </MuiNextLink>
+        <MuiNextLink href="/privacy-policy">
           <Policy fontSize="large" />
-        </Box>
+        </MuiNextLink>
       </Stack>
       {/* TODO: add footer navigation */}
-      <Typography align="center" color="common.white">
+      <Typography align="center" color="primary">
         © 2013 - {new Date().getFullYear()}, Stone Innovations Enterprise
       </Typography>
     </Box>
